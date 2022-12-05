f9c7f5ff58a92741761af2fcae2decb8c22d27b0
공용 <@yoonsubin>
2022-12-05 13:50:14 +0900

    commit remove workout

--- gymsystem/src/main/java/model/Reservation.java
+++ gymsystem/src/main/java/model/Reservation.java
@@ -8,20 +8,16 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class Reservation implements Serializable {
-	private int resId; 
+	private int resId; //예약id
 	private int trainerId; //강사번호
 	private String exerciseName; //운동 이름 및 종목
-	private int exercisePrice; //운동가격
-	private String payStatus; //결제상태
 	private String resStatus; //운동예약 상태
 	
-	public Reservation(int resId,int trainerId, int exercisePrice, String exerciseName, String payStatus, String resStatus) {
+	public Reservation(int resId,int trainerId, String exerciseName,  String resStatus) {
 		super();
 		this.resId=resId;
 		this.trainerId = trainerId;
-		this.exercisePrice=exercisePrice;
 		this.exerciseName = exerciseName;
-		this.payStatus = payStatus;
 		this.resStatus = resStatus;
 	}
 
@@ -49,22 +45,6 @@ public class Reservation implements Serializable {
 
 	public void setExerciseName(String exerciseName) {
 		this.exerciseName = exerciseName;
-	}
-
-	public int getExercisePrice() {
-		return exercisePrice;
-	}
-
-	public void setExercisePrice(int exercisePrice) {
-		this.exercisePrice = exercisePrice;
-	}
-
-	public String getPayStatus() {
-		return payStatus;
-	}
-
-	public void setPayStatus(String payStatus) {
-		this.payStatus = payStatus;
 	}
 
 	public String getResStatus() {
